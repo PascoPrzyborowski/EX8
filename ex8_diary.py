55a81fcb8bf04f6db10aaeb37ca873ea9421aea4
PascoPrzyborowski <unity01@gmx.net>
2022-08-25 14:06:53 +0200

    ex8

--- ex8_diary.py
+++ ex8_diary.py
@@ -9,7 +9,6 @@ import smtplib                          #opt and advanced
 from email.mime.multipart import *      #opt and advanced
 from email.mime.text      import *      #opt and advanced
 from email.message        import *
-from xmlrpc.client import NOT_WELLFORMED_ERROR      #opt and advanced
 from   colorama import *                #some color    
 from   art      import *                #asci art
 
@@ -30,17 +29,63 @@ def gmail_send(subject, message, from_mail, to_mail, password):
     msg['To']      = to_mail
     server.send_message(msg)
 
+print()
+print(Fore.LIGHTCYAN_EX+"")
+tprint("SECRET DIARY", font="random") #Logo
+print(""+Style.RESET_ALL)
+print()
+time.sleep(1)
 
+print("Loading ....")
+time.sleep(1)
+print()
 new_entry = input("Enter your Diary Entry: >>> ")
+print()
+time.sleep(1)
+
+#Some Deko stuff
 log = "Log Entry: "
-now = datetime.datetime.today().strftime("%H:%M:%S %d-%m-%Y")
-print(now)
+timestamp = "Your Local Time an Date is now:"
+now = datetime.datetime.today().strftime( "%H:%M:%S %d-%m-%Y")
+print(Fore.LIGHTGREEN_EX+"")
+print(timestamp + " " + now)
+print(""+Style.RESET_ALL)
+time.sleep(1)
+print()
 
-with open("copy.txt", "a") as file:
-    file.write(now + " " + log +" " + new_entry + "\n")
+
+with open("diary.txt", "a") as file:
+     file.write(now + " " + log +" " + new_entry + "\n")
+
+#reading part with Deko
+print("Loading your Entries, please wait ...")
+time.sleep(1)
+print()
+with open('diary.txt') as f:
+    print(Fore.LIGHTRED_EX+"")
+    entry = "You have the following Entries in your Diary: "
+    print(entry + "\n")
+    contents = f.read()
+    print(contents)
+    print(""+Style.RESET_ALL)
 
 
+#if new_entry != " ":  # If not empty print in Diary 
+#else:
+   # print("Empty Sting. -Not Adding- !")
 
+
+# print("Want to delete a line ?")
+# choice = input("yes for yes or not for not:")
+
+# if choice == "yes":
+#     with open("diary.txt", "r") as f:
+#         lines = f.readlines()
+#         delitem = input("Put the word in to delete:")
+#     with open("diary.txt", "w") as f:
+#         for line in lines:
+#             if line.strip(delitem + "\n"):
+#                 f.write(line)
 
 
 
